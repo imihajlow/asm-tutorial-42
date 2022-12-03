73e077dbfb09758b6840bc9919321752b9e49495
Ivan Mikhailov <imihajlow@gmail.com>
2022-12-03 10:30:45 +0100

    Indent

--- 01-asm-main/hello.asm
+++ 01-asm-main/hello.asm
@@ -1,21 +1,18 @@
-    bits 64
-    default rel
-    
-    global main           ; symbol main is exported
-    extern write          ; symbol write is imported
+            bits    64
+            default rel
 
-    section .text
-main:                     
-    mov edi, 1            ;  first parameter in EDI - int fd           32 bits
-    mov rsi, hello        ; second parameter in RSI - const void *buf  64 bits
-    mov edx, hello_len    ;  third parameter in RDX - size_t count     64 bits
-    call write
+            global  main           ; symbol main is exported
+            extern  write          ; symbol write is imported
 
-    xor eax, eax          ; main() return value in EAX                 32 bits
-    ret
+            section .text
+main:       mov     edi, 1            ;  first parameter in EDI - int fd           32 bits
+            mov     rsi, hello        ; second parameter in RSI - const void *buf  64 bits
+            mov     edx, hello_len    ;  third parameter in RDX - size_t count     64 bits
+            call    write
 
-    section .rodata
-hello:
-    db "Hello from ASM main", 10
+            xor     eax, eax          ; main() return value in EAX                 32 bits
+            ret
 
-hello_len equ $ - hello
+            section .rodata
+hello:      db      "Hello from ASM main", 10
+hello_len:  equ     $ - hello
